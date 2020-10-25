c643fdaba17e047cd507ac6c7d142b5a66d48100
cyee <chee.yee@home.net>
2020-10-25 12:52:56 -0700

    add initial ime changes

---
+++ src/ime_dialog.h
@@ -0,0 +1,39 @@
+/*
+  VitaShell
+  Copyright (C) 2015-2018, TheFloW
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __IME_DIALOG_H__
+#define __IME_DIALOG_H__
+
+#define IME_DIALOG_RESULT_NONE 0
+#define IME_DIALOG_RESULT_RUNNING 1
+#define IME_DIALOG_RESULT_FINISHED 2
+#define IME_DIALOG_RESULT_CANCELED 3
+
+#define IME_DIALOG_ALREADY_RUNNING -1
+
+namespace Dialog
+{
+    int initImeDialog(const char *title, const char *initial_text, int max_text_length, int type, int option, int password);
+    uint16_t *getImeDialogInputTextUTF16();
+    uint8_t *getImeDialogInputTextUTF8();
+    const char *getImeDialogInitialText();
+    int isImeDialogRunning();
+    int updateImeDialog();
+}
+
+#endif
