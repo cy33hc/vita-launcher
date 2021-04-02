122ef1f97746d76e1cc0d1640fbea5d2349b7b1e
cy33hc <cheeyee09@gmail.com>
2021-04-02 00:00:37 -0700

    increase ini max buffer to 4096

--- src/inifile.h
+++ src/inifile.h
@@ -42,7 +42,7 @@ typedef struct
    struct ENTRY *pSec;
    struct ENTRY *pKey;
    char          KeyText [128];
-   char          ValText [2048];
+   char          ValText [4096];
    char          Comment [32];
 } EFIND;
 
